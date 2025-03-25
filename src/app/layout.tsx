0f0fec3786009fdd31414ea325fbbf9c59dda5a2
nemanihemalatha <nemanihemalatha@gmail.com>
2025-03-25 19:00:18 +0530

    adjusted the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,23 +48,25 @@ export default function RootLayout({
   const SIDEBAR_WIDTH:number = isHoverEnabled ? 60 : 240;
   const isLoginPage = pathname === '/'
 
-  const [user, setUser] = useState<User | null>(null);
-
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {  
-      const storedUser = localStorage.getItem("user");  
-      if (storedUser) {
-        try {
-          const parsedUser: User = JSON.parse(storedUser);
-          setUser(parsedUser);
-        } catch (error) {
-          console.error("Error parsing user data", error);
-          setUser(null);
-        }
+  const getUserFromStorage = (): User | null => {
+    if (typeof window !== "undefined") {
+      const storedUser = localStorage.getItem("user");
+      try {
+        return storedUser ? JSON.parse(storedUser) : null;
+      } catch (error) {
+        console.error("Error parsing user data", error);
+        return null;
       }
     }
+    return null;
+  };
+  
+  const [user, setUser] = useState<User | null>(getUserFromStorage);
+  
+  useEffect(() => {
+    setUser(getUserFromStorage());
   }, []);
+  
   
   
 
