47533c5f9181e70c279e21f985ceaceae6205fe8
nemanihemalatha <nemanihemalatha@gmail.com>
2025-03-29 17:59:28 +0530

    changes updated like table size loading etc5

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api",
-  // baseURL: "https://api.ogaenik.com/api",
+  // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api",
+  baseURL: "https://api.ogaenik.com/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
